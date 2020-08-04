67c44732a62c121a70835b99db6180fba563ce79
Momoh Nobert <okhai0112@gmail.com>
2020-08-04 18:27:17 +0100

    minor changes

--- src/components/entry-view/introduction/introduction.component.jsx
+++ src/components/entry-view/introduction/introduction.component.jsx
@@ -8,7 +8,6 @@ const Introduction = () => (
             <h1 className="introduction-text-h1">Hello, World</h1>
             <h2 className="introduction-text-first-h2">I'm Nobert.</h2>
             <h2 className="introduction-text-second-h2">Full Stack Developer.</h2>
-            <h2 className="introduction-text-second-h2">Based in Abuja, Nigeria.</h2>
             <h3 className="introduction-text-h3">Bearing a range of skills in Design, Writing and Programming,</h3>
             <h3 className="introduction-text-h3">Coupled with a natural inclination for learning and self development.</h3>
             <h3 className="introduction-text-h3">I can help you spin your way brilliantly out of the modern day Web.</h3>
